2d178a85fd388edd8d7c68c8782db4a6df0e7425
Mykola Zhebet <phpapple@gmail.com>
2025-11-18 14:30:33 +0200

    stop button for chat response by using stop hook(abortController.abort signal for fetch)

--- src/app/ui/completion/stream/page.tsx
+++ src/app/ui/completion/stream/page.tsx
@@ -1,28 +1,20 @@
 'use client'
-import React, { useState } from "react"
 import { useCompletion } from "@ai-sdk/react";
 export default function StreamPage() { 
-    const { input, handleInputChange, handleSubmit, completion, isLoading, error } = useCompletion({
+    const { input, handleInputChange, handleSubmit, completion, isLoading, error, stop } = useCompletion({
         api: '/api/completion/stream',
     });
-
-    // const [prompt, setPrompt] = useState(''); //User input
-    // const [completion, setCompletion] = useState('');//AI response
-    // const [isLoading, setIsLoading] = useState(false);
-    // const [error, setError] = useState<string | null>(null);
-
     
     return (
         <div>
             {/** Display completion */}
             {isLoading && !completion && (<div>...Loading</div>)}
             {
-                completion ? (
+            completion ? (
                         <div>Streaming completion: {completion}</div>
-                ) : 'No response from AI'
+                     ) : 'No response from AI'
             }
             {error && <div>Error message: {error.message}</div>}
-            {/* <form onSubmit={handleSubmit}> */}
             <form onSubmit={(e) => {
                 e.preventDefault();
                 console.log('handle submit is invoked');
@@ -31,6 +23,8 @@ export default function StreamPage() {
                 <div>
                     <input placeholder="How can I help you" type="text" value={input} onChange={handleInputChange}></input>
                     <button type="submit" disabled={isLoading}>Send</button>
+                    <br/>
+                    {isLoading && (<button onClick={stop}>Stop waiting response</button>)}
                 </div>
             </form>
         </div>
