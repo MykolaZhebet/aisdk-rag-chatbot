348e678f425f1b9b11387ea3c17ad44ef2c6069e
Mykola Zhebet <phpapple@gmail.com>
2025-11-22 12:09:42 +0200

    add handling of different toll outut states for  more transparent UI representation

--- src/app/ui/tools/page.tsx
+++ src/app/ui/tools/page.tsx
@@ -3,11 +3,12 @@
 import React, { useState } from "react";
 import { useChat } from "@ai-sdk/react";
 import { DefaultChatTransport } from 'ai';
-
+// import type { ChatMessage } from "@/app/api/chat/route";
+import type { ChatMessage } from "@/app/api/tools/route";
 
 export default function ToolsChatPage() { 
      
-    const { messages, sendMessage, status, error, stop } = useChat({
+    const { messages, sendMessage, status, error, stop } = useChat<ChatMessage>({
         transport: new DefaultChatTransport({
             api: '/api/tools',
         }),
@@ -30,13 +31,41 @@ export default function ToolsChatPage() {
                 messages.map((message) => (
                         <div key={message.id}>
                             <div>{message.role === 'user' ? 'You' : 'AI'}:</div>
-                            <div>{message.parts.map((part, index) => {
+                        <div>{message.parts.map((part, index) => {
                                 switch (part.type) {
                                     case "text":
-                                        return <div key={`${message.id}-${index}`}>{part.text}</div>
+                                        return <div key={`${message.id}-${index}`}>{part.text}</div>;
+                                    case "tool-getWeather":
+                                        switch (part.state) { 
+                                            case "input-streaming":
+                                                return (<div key={`${message.id}-getWeather-${index}`}>
+                                                    <p>Weather forecast</p>
+                                                    <pre>
+                                                        {JSON.stringify(part.input, null, 2)}
+                                                    </pre>
+                                                </div>)
+                                            case 'input-available':
+                                                return (<div key={`${message.id}-getWeather-${index}`}>
+                                                    <p>Getting weather for { part.input.city}</p>
+                                                </div>);
+                                            case 'output-available':
+                                                return (<div key={`${message.id}-getWeather-${index}`}>
+                                                        Weather is {part.output}
+                                                </div>)
+                                            case 'output-error':
+                                                return (<div key={`${message.id}-getWeather-${index}`}>
+                                                    Error during getWeather tool output: {part.errorText}
+                                                </div>)
+                                            default:
+                                                return (<div key={`${message.id}-getWeather-${index}`}>
+                                                            Unknown weather tool output: {JSON.stringify(part, null, 2)}
+                                                        </div>
+                                                )
+                                        }
                                     default:
-                                        return null;
-                                    }
+                                        return (<p>Unknown output type: {JSON.stringify(message, null, 2)} </p>);
+                                }
+                                
                             })}</div>
                             <br />
                         </div>
