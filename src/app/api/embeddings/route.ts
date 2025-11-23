ed0ca709b5d3ea40295b971db9286013a2930cfa
Mykola Zhebet <phpapple@gmail.com>
2025-11-23 16:48:26 +0200

    example of butch embedding for array of texts

--- src/app/api/embeddings/route.ts
+++ src/app/api/embeddings/route.ts
@@ -5,20 +5,52 @@ import { config } from "dotenv";
 
 config({ path: ".env.local" });
 export async function POST(req: Request) {
-    const { text } = await req.json();
+    // const { text } = await req.json();
+    const body = await req.json();
     // const { embedding } = await embed({
     const result = await embed({
         // model: ollama.textEmbeddingModel("nomic-embed-text:v1.5")
         model: ollama.textEmbeddingModel(process.env.EMBEDDING_MODEL!),
         // value: 'A movie about hackers discovering reality in a simulation'
-        value: text
+        // value: text
+        value: body.texts  
     });
+
+
+    if (Array.isArray(body.texts)) {
+    // const { embedding } = await embed({
+        const result = await embedMany({
+            // model: ollama.textEmbeddingModel("nomic-embed-text:v1.5")
+            model: ollama.textEmbeddingModel(process.env.EMBEDDING_MODEL!),
+            // value: 'A movie about hackers discovering reality in a simulation'
+            // value: text
+            values: body.texts,
+            maxParallelCalls: 5,//process 5 embedding at the time
+        });    
+         return Response.json({
+            values: result.values,
+            embeddings: result.embeddings,
+             usage: result.usage,
+            count: result.embeddings.length,
+            dimansions: result.embeddings[0].length
+        })
+    } else {
+        // const { embedding } = await embed({
+        const result = await embed({
+            // model: ollama.textEmbeddingModel("nomic-embed-text:v1.5")
+            model: ollama.textEmbeddingModel(process.env.EMBEDDING_MODEL!),
+            // value: 'A movie about hackers discovering reality in a simulation'
+            // value: text
+            value: body.text  
+        });
+         return Response.json({
+            value: result.value,
+            embedding: result.embedding,
+            usage: result.usage,
+            dimansions: result.embedding.length
+        })
+    }
     // return Response.json({embedding})
     // return Response.json(result)
-    return Response.json({
-        value: result.value,
-        embedding: result.embedding,
-        usage: result.usage,
-        dimansions: result.embedding.length
-    })
+   
 }
